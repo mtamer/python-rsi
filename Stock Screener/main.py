bc9c9919e4e4689b8ad0e6a24b2292c63fb19b02
corncobb <cameronjcobb@gmail.com>
2020-07-16 01:01:12 -0700

    converted methods into Stock class and simplified some methods, updated and improved naming conventions

--- Stock Screener/main.py
+++ Stock Screener/main.py
@@ -13,6 +13,8 @@ import numpy as np
 import pandas_datareader.data as web
 import pylab
 from mplfinance.original_flavor import candlestick_ohlc
+from pandas.core.common import flatten
+from tabulate import tabulate
 
 matplotlib.rcParams.update({'font.size': 9})
 
@@ -23,224 +25,279 @@ stocks = stocks if len(stocks) > 0 else [
     line.rstrip() for line in open("stocks.txt", "r")]
 
 
-def RSI(prices, n=14):
-    deltas = np.diff(prices)
-    seed = deltas[:n+1]
-    up = seed[seed >= 0].sum()/n
-    down = -seed[seed < 0].sum()/n
-    rs = up/down
-    rsi = np.zeros_like(prices)
-    rsi[:n] = 100. - 100./(1.+rs)
+class Stock:
 
-    for i in range(n, len(prices)):
-        delta = deltas[i-1]  # The diff is 1 shorter
+    ticker = None
+    dates = None
+    closes = None
+    highs = None
+    lows = None
+    opens = None
+    volumes = None
+    rsi = None
 
-        if delta > 0:
-            upval = delta
-            downval = 0.
-        else:
-            upval = 0.
-            downval = -delta
+    def __init__(self, ticker, start, end=datetime.datetime.now()):
+        self.ticker = ticker
 
-        up = (up*(n-1) + upval)/n
-        down = (down*(n-1) + downval)/n
+        """
+        Different sources for pulling data can be found here:
+        https://readthedocs.org/projects/pandas-datareader/downloads/pdf/latest/
+        """
 
+        stockData = web.DataReader(ticker, 'yahoo', start, end)
+
+        self.dates = [mdates.date2num(d) for d in stockData.index]
+        self.closes = stockData['Close']
+        self.highs = stockData['High']
+        self.lows = stockData['Low']
+        self.opens = stockData['Open']
+        self.volumes = stockData['Volume']
+
+        self.rsi = self.RSI(self.closes)
+
+    def RSI(self, prices, n=14):
+        deltas = np.diff(prices)
+        seed = deltas[:n+1]
+        up = seed[seed >= 0].sum()/n
+        down = -seed[seed < 0].sum()/n
         rs = up/down
-        rsi[i] = 100. - 100./(1.+rs)
+        rsi = np.zeros_like(prices)
+        rsi[:n] = 100. - 100./(1.+rs)
 
-    return rsi
+        for i in range(n, len(prices)):
+            delta = deltas[i-1]  # The diff is 1 shorter
 
+            if delta > 0:
+                upval = delta
+                downval = 0.
+            else:
+                upval = 0.
+                downval = -delta
 
-def movingAverage(values, window):
-    weigths = np.repeat(1.0, window)/window
-    smas = np.convolve(values, weigths, 'valid')
-    return smas  # as a numpy array
+            up = (up*(n-1) + upval)/n
+            down = (down*(n-1) + downval)/n
 
+            rs = up/down
+            rsi[i] = 100. - 100./(1.+rs)
 
-def expMovingAverage(values, window):
-    weights = np.exp(np.linspace(-1., 0., window))
-    weights /= weights.sum()
-    a = np.convolve(values, weights, mode='full')[:len(values)]
-    a[:window] = a[window]
-    return a
+        return rsi
 
+    def SMA(self, period, values=None):
 
-def MACD(x, slow=26, fast=12):
-    """
-    Compute the MACD (Moving Average Convergence/Divergence) using a fast and slow exponential moving avg'
-    return value is emaslow, emafast, macd which are len(x) arrays
-    """
-    emaslow = expMovingAverage(x, slow)
-    emafast = expMovingAverage(x, fast)
-    return emaslow, emafast, emafast - emaslow
+        values = self.closes if values is None else values
 
+        """
+        Simple Moving Average. Periods are the time frame. For example, a period of 50 would be a 50 day
+        moving average. Values are usually the stock closes but can be passed any values
+        """
 
-def queryStockInfo(stock, start, end):
+        weigths = np.repeat(1.0, period)/period
+        smas = np.convolve(values, weigths, 'valid')
+        return smas  # as a numpy array
 
-    try:
+    def EMA(self, period, values=None):
 
-        # Potentially add other methods for pulling stock data like TIINGO, etc.
-        data = web.DataReader(stock, 'yahoo', start, end)
+        values = self.closes if values is None else values
 
-        return data
+        """
+        Exponential Moving Average. Periods are the time frame. For example, a period of 50 would be a 50 day
+        moving average. Values are usually the stock closes but can be passed any values
+        """
 
-    except Exception as e:
-        print(str(e), 'failed to pull pricing data')
+        weights = np.exp(np.linspace(-1., 0., period))
+        weights /= weights.sum()
+        a = np.convolve(values, weights, mode='full')[:len(values)]
+        a[:period] = a[period]
+        return a
 
+    def MACD(self, x, slow=26, fast=12):
+        """
+        Compute the MACD (Moving Average Convergence/Divergence) using a fast and slow exponential moving avg'
+        return value is emaslow, emafast, macd which are len(x) arrays
+        """
 
-def graphData(stock, stockData, rsi, movingAverageArr):
-    try:
-        date = [mdates.date2num(d) for d in stockData.index]
-        closep = stockData['Close']
-        highp = stockData['High']
-        lowp = stockData['Low']
-        openp = stockData['Open']
-        volume = stockData['Volume']
+        emaslow = self.EMA(slow, x)
+        emafast = self.EMA(fast, x)
+        return emaslow, emafast, emafast - emaslow
 
-        x = 0
-        y = len(date)
-        newAr = []
-        while x < y:
-            appendLine = date[x], openp[x], closep[x], highp[x], lowp[x], volume[x]
-            newAr.append(appendLine)
-            x += 1
+    def graph(self, movingAverageArr=[]):
+        try:
 
-        # Fix this
-        SP = len(date[200-1:])
+            x = 0
+            y = len(self.dates)
+            newAr = []
+            while x < y:
+                appendLine = self.dates[x], self.opens[x], self.closes[x], self.highs[x], self.lows[x], self.volumes[x]
+                newAr.append(appendLine)
+                x += 1
 
-        fig = plt.figure(facecolor='#07000d')
+            # Fix this
+            SP = len(self.dates[200-1:])
 
-        ax1 = plt.subplot2grid(
-            (6, 4), (1, 0), rowspan=4, colspan=4, facecolor='#07000d')
-        candlestick_ohlc(ax1, newAr[-SP:], width=.6,
-                         colorup='#53c156', colordown='#ff1717')
+            fig = plt.figure(facecolor='#07000d')
 
-        for MA in movingAverageArr:
-            computedMA = movingAverage(stockClose, MA)
+            ax1 = plt.subplot2grid(
+                (6, 4), (1, 0), rowspan=4, colspan=4, facecolor='#07000d')
+            candlestick_ohlc(ax1, newAr[-SP:], width=.6,
+                             colorup='#53c156', colordown='#ff1717')
 
-            # Used to generate random hex color to put on graph
-            def r(): return random.randint(0, 255)
-            randomColor = '#%02X%02X%02X' % (r(), r(), r())
+            for MA in movingAverageArr:
 
-            maLabel = str(MA) + ' SMA'
-            ax1.plot(date[-SP:], computedMA[-SP:], randomColor,
-                     label=maLabel, linewidth=1.5)
+                computedSMA = self.SMA(MA, self.closes)
 
-        ax1.grid(False, color='w')
-        ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        ax1.yaxis.label.set_color("w")
-        ax1.spines['bottom'].set_color("#5998ff")
-        ax1.spines['top'].set_color("#5998ff")
-        ax1.spines['left'].set_color("#5998ff")
-        ax1.spines['right'].set_color("#5998ff")
-        ax1.tick_params(axis='y', colors='w')
-        plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
-        ax1.tick_params(axis='x', colors='w')
-        plt.ylabel('Stock price and Volume')
+                # Used to generate random hex color to put on graph
+                def r(): return random.randint(0, 255)
+                randomColor = '#%02X%02X%02X' % (r(), r(), r())
 
-        maLeg = plt.legend(loc=9, ncol=2, prop={'size': 7},
-                           fancybox=True, borderaxespad=0.)
-        maLeg.get_frame().set_alpha(0.4)
-        textEd = pylab.gca().get_legend().get_texts()
-        pylab.setp(textEd[0:5], color='w')
+                maLabel = str(MA) + ' SMA'
+                ax1.plot(self.dates[-SP:], computedSMA[-SP:], randomColor,
+                         label=maLabel, linewidth=1.5)
 
-        volumeMin = 0
+            ax1.grid(False, color='w')
+            ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
+            ax1.xaxis.set_major_formatter(
+                mdates.DateFormatter('%Y-%m-%d'))
+            ax1.yaxis.label.set_color("w")
+            ax1.spines['bottom'].set_color("#5998ff")
+            ax1.spines['top'].set_color("#5998ff")
+            ax1.spines['left'].set_color("#5998ff")
+            ax1.spines['right'].set_color("#5998ff")
+            ax1.tick_params(axis='y', colors='w')
+            plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
+            ax1.tick_params(axis='x', colors='w')
+            plt.ylabel('Stock price and Volume')
 
-        ax0 = plt.subplot2grid(
-            (6, 4), (0, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
+            maLeg = plt.legend(loc=9, ncol=2, prop={'size': 7},
+                               fancybox=True, borderaxespad=0.)
+            maLeg.get_frame().set_alpha(0.4)
+            textEd = pylab.gca().get_legend().get_texts()
+            pylab.setp(textEd[0:5], color='w')
 
-        rsiCol = '#c1f9f7'
-        posCol = '#386d13'
-        negCol = '#8f2020'
+            volumeMin = 0
 
-        ax0.plot(date[-SP:], rsi[-SP:], rsiCol, linewidth=1.5)
-        ax0.axhline(70, color=negCol)
-        ax0.axhline(30, color=posCol)
-        ax0.fill_between(date[-SP:], rsi[-SP:], 70, where=(rsi[-SP:]
-                                                           >= 70), facecolor=negCol, edgecolor=negCol, alpha=0.5)
-        ax0.fill_between(date[-SP:], rsi[-SP:], 30, where=(rsi[-SP:]
-                                                           <= 30), facecolor=posCol, edgecolor=posCol, alpha=0.5)
-        ax0.set_yticks([30, 70])
-        ax0.yaxis.label.set_color("w")
-        ax0.spines['bottom'].set_color("#5998ff")
-        ax0.spines['top'].set_color("#5998ff")
-        ax0.spines['left'].set_color("#5998ff")
-        ax0.spines['right'].set_color("#5998ff")
-        ax0.tick_params(axis='y', colors='w')
-        ax0.tick_params(axis='x', colors='w')
-        plt.ylabel('RSI')
+            ax0 = plt.subplot2grid(
+                (6, 4), (0, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
 
-        ax1v = ax1.twinx()
-        ax1v.fill_between(date[-SP:], volumeMin,
-                          volume[-SP:], facecolor='#00ffe8', alpha=.4)
-        ax1v.axes.yaxis.set_ticklabels([])
-        ax1v.grid(False)
-        # Edit this to 3, so it's a bit larger
-        ax1v.set_ylim(0, 3*volume.max())
-        ax1v.spines['bottom'].set_color("#5998ff")
-        ax1v.spines['top'].set_color("#5998ff")
-        ax1v.spines['left'].set_color("#5998ff")
-        ax1v.spines['right'].set_color("#5998ff")
-        ax1v.tick_params(axis='x', colors='w')
-        ax1v.tick_params(axis='y', colors='w')
-        ax2 = plt.subplot2grid(
-            (6, 4), (5, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
-        fillcolor = '#00ffe8'
-        nslow = 26
-        nfast = 12
-        nema = 9
-        emaslow, emafast, macd = MACD(closep)
-        ema9 = expMovingAverage(macd, nema)
-        ax2.plot(date[-SP:], macd[-SP:], color='#4ee6fd', lw=2)
-        ax2.plot(date[-SP:], ema9[-SP:], color='#e1edf9', lw=1)
-        ax2.fill_between(date[-SP:], macd[-SP:]-ema9[-SP:], 0,
-                         alpha=0.5, facecolor=fillcolor, edgecolor=fillcolor)
+            rsiCol = '#c1f9f7'
+            posCol = '#386d13'
+            negCol = '#8f2020'
 
-        plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
-        ax2.spines['bottom'].set_color("#5998ff")
-        ax2.spines['top'].set_color("#5998ff")
-        ax2.spines['left'].set_color("#5998ff")
-        ax2.spines['right'].set_color("#5998ff")
-        ax2.tick_params(axis='x', colors='w')
-        ax2.tick_params(axis='y', colors='w')
-        plt.ylabel('MACD', color='w')
-        ax2.yaxis.set_major_locator(
-            mticker.MaxNLocator(nbins=5, prune='upper'))
-        for label in ax2.xaxis.get_ticklabels():
-            label.set_rotation(45)
+            ax0.plot(self.dates[-SP:], self.rsi[-SP:],
+                     rsiCol, linewidth=1.5)
+            ax0.axhline(70, color=negCol)
+            ax0.axhline(30, color=posCol)
+            ax0.fill_between(self.dates[-SP:], self.rsi[-SP:], 70, where=(self.rsi[-SP:]
+                                                                          >= 70), facecolor=negCol, edgecolor=negCol, alpha=0.5)
+            ax0.fill_between(self.dates[-SP:], self.rsi[-SP:], 30, where=(self.rsi[-SP:]
+                                                                          <= 30), facecolor=posCol, edgecolor=posCol, alpha=0.5)
+            ax0.set_yticks([30, 70])
+            ax0.yaxis.label.set_color("w")
+            ax0.spines['bottom'].set_color("#5998ff")
+            ax0.spines['top'].set_color("#5998ff")
+            ax0.spines['left'].set_color("#5998ff")
+            ax0.spines['right'].set_color("#5998ff")
+            ax0.tick_params(axis='y', colors='w')
+            ax0.tick_params(axis='x', colors='w')
+            plt.ylabel('RSI')
 
-        plt.suptitle(stock.upper(), color='w')
+            ax1v = ax1.twinx()
+            ax1v.fill_between(self.dates[-SP:], volumeMin,
+                              self.volumes[-SP:], facecolor='#00ffe8', alpha=.4)
+            ax1v.axes.yaxis.set_ticklabels([])
+            ax1v.grid(False)
 
-        plt.setp(ax0.get_xticklabels(), visible=False)
-        plt.setp(ax1.get_xticklabels(), visible=False)
+            # Edit this to 3, so it's a bit larger
+            ax1v.set_ylim(0, 3*self.volumes.max())
+            ax1v.spines['bottom'].set_color("#5998ff")
+            ax1v.spines['top'].set_color("#5998ff")
+            ax1v.spines['left'].set_color("#5998ff")
+            ax1v.spines['right'].set_color("#5998ff")
+            ax1v.tick_params(axis='x', colors='w')
+            ax1v.tick_params(axis='y', colors='w')
+            ax2 = plt.subplot2grid(
+                (6, 4), (5, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
+            fillcolor = '#00ffe8'
+            nslow = 26
+            nfast = 12
+            nema = 9
+            emaslow, emafast, macd = self.MACD(self.closes)
+            ema9 = self.EMA(nema, macd)
+            ax2.plot(self.dates[-SP:], macd[-SP:],
+                     color='#4ee6fd', lw=2)
+            ax2.plot(self.dates[-SP:], ema9[-SP:],
+                     color='#e1edf9', lw=1)
+            ax2.fill_between(self.dates[-SP:], macd[-SP:]-ema9[-SP:], 0,
+                             alpha=0.5, facecolor=fillcolor, edgecolor=fillcolor)
 
-        # ax1.annotate('Look Here!', (date[-1], Av1[-1]),
-        #              xytext=(0.8, 0.9), textcoords='axes fraction',
-        #              arrowprops=dict(facecolor='white', shrink=0.05),
-        #              fontsize=14, color='w',
-        #              horizontalalignment='right', verticalalignment='bottom')
+            plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
+            ax2.spines['bottom'].set_color("#5998ff")
+            ax2.spines['top'].set_color("#5998ff")
+            ax2.spines['left'].set_color("#5998ff")
+            ax2.spines['right'].set_color("#5998ff")
+            ax2.tick_params(axis='x', colors='w')
+            ax2.tick_params(axis='y', colors='w')
+            plt.ylabel('MACD', color='w')
+            ax2.yaxis.set_major_locator(
+                mticker.MaxNLocator(nbins=5, prune='upper'))
+            for label in ax2.xaxis.get_ticklabels():
+                label.set_rotation(45)
 
-        plt.subplots_adjust(left=.09, bottom=.14,
-                            right=.94, top=.95, wspace=.20, hspace=0)
+            plt.suptitle(self.ticker.upper(), color='w')
 
-        plt.show()
-        fig.savefig('example.png', facecolor=fig.get_facecolor())
+            plt.setp(ax0.get_xticklabels(), visible=False)
+            plt.setp(ax1.get_xticklabels(), visible=False)
 
-    except Exception as e:
-        print('graphData error: ', str(e))
+            # ax1.annotate('Look Here!', (date[-1], Av1[-1]),
+            #              xytext=(0.8, 0.9), textcoords='axes fraction',
+            #              arrowprops=dict(facecolor='white', shrink=0.05),
+            #              fontsize=14, color='w',
+            #              horizontalalignment='right', verticalalignment='bottom')
+
+            plt.subplots_adjust(left=.09, bottom=.14,
+                                right=.94, top=.95, wspace=.20, hspace=0)
+
+            plt.show()
+            fig.savefig('example.png', facecolor=fig.get_facecolor())
+
+        except Exception as e:
+            print('Error graphing data: ', str(e))
 
 
 if __name__ == "__main__":
     start = datetime.datetime.now()-datetime.timedelta(days=365)
     end = datetime.datetime.now()
 
-    for stock in stocks:
-        print('Currently Pulling', stock)
-        stockInfo = queryStockInfo(stock, start, end)
-        stockClose = stockInfo['Close']
-        stockRsi = RSI(stockClose)
+    # Array of moving averages you want to get
+    MAarr = [20, 200]
 
-        if stockRsi[-1] < 30 or stockRsi[-1] > 70:
+    for ticker in stocks:
+        data = []
 
-            graphData(stock, stockInfo, stockRsi, [20, 200])
+        #print('Currently Pulling', ticker)
+        stock = Stock(ticker, start, end)
+
+        # Append data to array
+        data.append(ticker.upper())
+
+        data.append(stock.closes[-1])
+        for MA in MAarr:
+            computedSMA = stock.SMA(period=MA)
+            # print(computedSMA)
+            data.append(computedSMA[-1])
+
+        currentRsi = float("{:.2f}".format(stock.rsi[-1]))
+
+        if currentRsi > 70:
+            data.append(str(currentRsi) + " 🔥")
+        elif currentRsi < 30:
+            data.append(str(currentRsi) + " 🧊")
+        else:
+            data.append(str(currentRsi))
+
+        print(tabulate([data], headers=flatten([
+              'Stock', 'Price', MAarr, "RSI"])))
+
+        # Shows chart only if current RSI is greater than or less than 70 or 30 respectively
+        if currentRsi < 30 or currentRsi > 70:
+
+            stock.graph(MAarr)
